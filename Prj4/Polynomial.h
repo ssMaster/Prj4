185cf70fe250bebd5f2d3aada91a55edc650f866
masterSZS <simon_sharudin1@msn.com>
2015-06-11 17:47:24 -0700

    Prj4 initial commit from local

---
+++ Prj4/Polynomial.h
@@ -0,0 +1,34 @@
+#ifndef POLYNOMIAL_H
+#define POLYNOMIAL_H
+
+#include <iostream>
+#include <iomanip>
+#include <cmath> 
+#include <cstring>
+using namespace std;
+
+class Polynomial {
+
+	friend istream& operator>>(istream& in, Polynomial& v);
+	friend ostream& operator<<(ostream& out, const Polynomial& v);
+	friend void show(Polynomial* p);
+
+public:
+	Polynomial();
+	Polynomial(const Polynomial&);
+	Polynomial(const char *);
+	Polynomial operator+(const Polynomial&) const;
+	Polynomial operator-(const Polynomial&) const;
+	Polynomial operator*(const Polynomial&);
+	Polynomial& operator+=(const Polynomial&);
+	Polynomial& operator-=(const Polynomial&);
+	Polynomial& operator*=(const Polynomial&);
+	int operator[](int) const;
+	int& operator[](int);
+	int operator()(int);
+
+private:
+	int coefficients_[10];
+};
+
+#endif
---
+++ Prj4/Prj4Test.cpp
@@ -0,0 +1,137 @@
+
+/*
+#include "polynomial.h"
+
+void show(Polynomial* p) {
+	cout << "Coefficients are: " << p->coefficients_[0];
+	for (int i = 1; i<10; i++) {
+		cout << "," << p->coefficients_[i];
+	}
+	cout << endl;
+}*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i, j;
+	for (i = 0, j = 0; i < 10; i++, ++j){
+		cout << "i is:\t" << i << " and j is: " << j << endl;
+	}
+	cout << "i is:\t" << i << " and j is: " << j << endl << endl;
+
+	i = j = 0;
+	cout << "i = " << i << endl;
+	cout << " and now i++ = " << i++ << endl;
+	cout << "j = " << j << " now ++j = " << ++j << endl;
+
+	// ============================================================================================
+
+	/*int i;
+
+	cout << "Start of default constructor, Polynomial(), tests..." << endl;
+	Polynomial *a;
+	Polynomial b, c;
+	a = new Polynomial();
+	show(a);
+	cout << "End of Polynomial() tests." << endl << endl;
+
+	cout << "Start of user-defined constructor, Polynomial(const char*), tests..." << endl;
+	a = new Polynomial("-12 + 7x2 + 7x9 - 21x5 - 11x7");
+	show(a);
+	delete a;
+
+	a = new Polynomial("-2x9 + 7x7 - 18x3 + 6x1 - 14");
+	show(a);
+	delete a;
+	cout << "End of Polynomial(const char*) tests." << endl << endl;
+
+	cout << "Start of >> and << tests..." << endl;
+	cout << "Please enter: 3x9 - 6x7 - 18x3 + 6x1 + 14" << endl;
+	cin >> b;
+	cout << b << endl;
+	cout << "Please enter: -4x7 + 12x4 + 9x1 - 9" << endl;
+	cin >> c;
+	cout << c << endl;
+	cout << "End of >> and << tests." << endl << endl;
+
+	cout << "Start of copy constructor tests..." << endl;
+	Polynomial d(b);
+	cout << d << endl;
+	Polynomial e(c);
+	cout << e << endl;
+	cout << "End of copy tests...." << endl << endl;
+
+	Polynomial f("3x9 - 6x7 - 18x3 + 6x1 + 14");
+	Polynomial g("-2x9 + 7x7 - 18x3 + 6x2 - 11");
+	Polynomial h("-4x7 + 12x4 + 9x1 - 9");
+
+	cout << "Start of + tests..." << endl;
+	e = f + g;
+	cout << e << endl;
+	e = f + h;
+	cout << e << endl;
+	cout << "End of + tests...." << endl << endl;
+
+	cout << "Start of - tests..." << endl;
+	e = f - g;
+	cout << e << endl;
+	e = f - h;
+	cout << e << endl;
+	cout << "End of - tests...." << endl << endl;
+
+	cout << "Start of * tests..." << endl;
+	e = f * g;
+	cout << e << endl;
+	e = f * h;
+	cout << e << endl;
+	cout << "End of * tests...." << endl << endl;
+
+	cout << "Start of += tests..." << endl;
+	e = f;
+	e += g;
+	cout << e << endl;
+	e = f;
+	e += h;
+	cout << e << endl;
+	cout << "End of += tests...." << endl << endl;
+
+	cout << "Start of -= tests..." << endl;
+	e = f;
+	e -= g;
+	cout << e << endl;
+	e = f;
+	e -= h;
+	cout << e << endl;
+	cout << "End of -= tests...." << endl << endl;
+
+	cout << "Start of *= tests..." << endl;
+	e = f;
+	e *= g;
+	cout << e << endl;
+	e = f;
+	e *= h;
+	cout << e << endl;
+	cout << "End of *= tests...." << endl << endl;
+
+	cout << "Start of [] tests..." << endl;
+	for (i = 9; i >= 0; i--)
+		cout << "e[" << setw(1) << i << "] = " << setw(1) << e[i] << "\t";
+	cout << endl;
+	for (i = 9; i >= 0; i--)
+		e[i] = i;
+	for (i = 9; i >= 0; i--)
+		cout << "e[" << setw(1) << i << "] = " << setw(1) << e[i] << "\t";
+	cout << endl;
+	cout << "End of [] tests...." << endl << endl;
+
+	cout << "Start of () tests..." << endl;
+	cout << "Evaluation = " << e(1) << endl;
+	cout << "Evaluation = " << e(4) << endl;
+	cout << "End of () tests...." << endl;*/
+
+	return 0;
+}
+
+
